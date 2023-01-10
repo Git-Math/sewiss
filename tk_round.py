de79b8d0a66b057a0e94cdc40abd5ee08f41ae8c
Mathieu N <matoo3@hotmail.fr>
2023-01-10 20:18:16 +0100

    tk_round: end implementation

--- tk_round.py
+++ tk_round.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from player import *
 
 MAX_MATCHES_IN_ROW = 2
 MATCH_COLUMN_SIZE = 3
 MATCH_ROW_SIZE = 4
 X_SPACE_BETWEEN_MATCHES = 50
-Y_SPACE_BETWEEN_MATCHES = 20
 X_SPACE_BEFORE_STANDINGS = 150
+ROW_MIN_SIZE = 20
 
 def read_round_results(matchups_widgets, round_results, error_label, root):
     for mw in matchups_widgets:
@@ -50,16 +51,44 @@ def swap_widgets(start_widget, target_widget):
     start_widget.config(text = target_text)
     target_widget.config(text = start_text)
 
-def on_click(event, players_labels, root):
+def find_matchup_widgets_by_player_widget(matchups_widgets, player_widget):
+    for matchup_widgets in matchups_widgets:
+        if matchup_widgets["player1"] == player_widget or matchup_widgets["player2"] == player_widget:
+            return matchup_widgets
+
+    return None
+
+def check_matchup_warning(player1, player2):
+    is_rematch = player1.is_rematch(player2)
+    same_set_count = player1.compare_set_count(player2)
+
+    warning_string = ""
+    warning_string += "Rematch" if is_rematch else ""
+    warning_string += (("/" if is_rematch else "") + "Set count diff") if not same_set_count else ""
+
+    return warning_string
+
+def update_matchup_widgets_warning(matchup_widgets, players):
+    player1 = find_player_by_name(players, matchup_widgets["player1"].cget("text"))
+    player2 = find_player_by_name(players,matchup_widgets["player2"].cget("text"))
+
+    warning_string = check_matchup_warning(player1, player2)
+
+    matchup_widgets["warning"].config(text = warning_string)
+
+def on_click(event, matchups_widgets, players, root):
     widget = event.widget
 
-    if widget in players_labels:
-        start_position = (event.x, event.y)
-        widget_grid_info = widget.grid_info()
-        widget.bind("<B1-Motion>", lambda event:drag_motion(event, widget, start_position))
-        widget.bind("<ButtonRelease-1>", lambda event:drag_release(event, players_labels, widget, widget_grid_info, root))
-    else:
+    matchup_widgets = find_matchup_widgets_by_player_widget(matchups_widgets, widget)
+
+    if not matchup_widgets:
         root.unbind("<ButtonRelease-1>")
+        return
+
+    start_position = (event.x, event.y)
+    widget_grid_info = widget.grid_info()
+    widget.bind("<B1-Motion>", lambda event:drag_motion(event, widget, start_position))
+    widget.bind("<ButtonRelease-1>", lambda event:drag_release(event, matchups_widgets, matchup_widgets, widget, widget_grid_info, players, root))
 
 def drag_motion(event, widget, start_position): 
     x = widget.winfo_x() + event.x - start_position[0]
@@ -67,12 +96,17 @@ def drag_motion(event, widget, start_position):
     widget.lift()
     widget.place(x = x, y = y)
 
-def drag_release(event, players_labels, widget, widget_grid_info, root):
+def drag_release(event, matchups_widgets, matchup_widgets, widget, widget_grid_info, players, root):
     widget.lower()
+    widget.config(state = "disabled")
     x, y = root.winfo_pointerxy()
     target_widget = root.winfo_containing(x, y)
-    if target_widget in players_labels:
+    widget.config(state = "enabled")
+    target_matchup_widgets = find_matchup_widgets_by_player_widget(matchups_widgets, target_widget)
+    if target_matchup_widgets:
         swap_widgets(widget, target_widget)
+        update_matchup_widgets_warning(matchup_widgets, players)
+        update_matchup_widgets_warning(target_matchup_widgets, players)
     widget.grid(row = widget_grid_info['row'], column = widget_grid_info['column'])
 
 def run(players, matchups, r):
@@ -107,18 +141,10 @@ def run(players, matchups, r):
         start_column = (i % MAX_MATCHES_IN_ROW) * MATCH_COLUMN_SIZE
         start_row = (int(i / MAX_MATCHES_IN_ROW) * MATCH_ROW_SIZE) + 1
 
-        if i % MAX_MATCHES_IN_ROW == 0:
-            frame.rowconfigure(start_row, minsize = Y_SPACE_BETWEEN_MATCHES)
+        warning_string = check_matchup_warning(player1, player2)
 
-        is_rematch = player1.is_rematch(player2)
-        same_set_count = player1.compare_set_count(player2)
-
-        warning_string = ""
-        warning_string += "Rematch" if is_rematch else ""
-        warning_string += (("/" if is_rematch else "") + "Set count diff") if not same_set_count else ""
-
-        warning_label = ttk.Label(frame, text = warning_string) \
-                           .grid(column = start_column, row = start_row + 1, columnspan = 2)
+        warning_label = ttk.Label(frame, text = warning_string)
+        warning_label.grid(column = start_column, row = start_row + 1, columnspan = 2)
         
         player1_label = ttk.Label(frame, text = player1.name, borderwidth = 10, relief="solid", width = 20);
         player1_label.grid(column = start_column, row = start_row + 2, sticky = tk.E)
@@ -175,7 +201,11 @@ def run(players, matchups, r):
            .grid(column = standings_start_column + 4, row = standings_start_row + 2 + i)
 
     # drag and drop button
-    #root.bind("<Button-1>", lambda event:on_click(event, players_labels, root))
+    root.bind("<Button-1>", lambda event:on_click(event, matchups_widgets, players, root))
+
+    # configure all rows min size
+    for row_i in range(frame.grid_size()[1]):
+        frame.rowconfigure(row_i, minsize = ROW_MIN_SIZE)
 
     root.mainloop()
 
