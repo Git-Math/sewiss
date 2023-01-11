1706b670ed27e6ef0d48f3685a130daa8585cb48
Mathieu N <matoo3@hotmail.fr>
2023-01-11 04:06:12 +0100

    sewiss: handle tournament name

--- sewiss.py
+++ sewiss.py
@@ -1,3 +1,4 @@
+import tk_tournament_name
 import tk_players_names
 import tk_players_seeds
 import tk_round
@@ -5,10 +6,15 @@ import tk_standings
 from player import *
 
 if __name__ == '__main__':
+    # tournament name
+    tournament_name = tk_tournament_name.run()
+
+    if not tournament_name:
+        print("Le nom du tournoi est vide")
+        exit()
+
     # players names
     players_names = tk_players_names.run()
-
-    print(players_names)
 
     if len(players_names) != 16:
         print("Il n'y a pas 16 noms de joueurs")
@@ -17,13 +23,9 @@ if __name__ == '__main__':
     # players seeding
     seeded_players_names = tk_players_seeds.run(players_names)
 
-    print(seeded_players_names)
-
     if len(seeded_players_names) != 16:
         print("Il n'y a pas 16 noms de joueurs")
         exit()
-
-    #seeded_players_names = ["Susu", "Dazed", "CHEF!", "Le Shibateur", "Suri", "Albertino", "Rico", "Manon", "Andreea", "Theo", "Logan", "Sirop", "Boris", "TBN", "Aness", "Marine"]
 
     players = []
 
